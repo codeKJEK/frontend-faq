b057b2a0d8ac8f85c2a6d67e2675bdce5c3eaae7
KJEKcode <kurtiskovel@gmail.com>
2021-11-10 18:04:31 -0500

    finialized HTML CSS. Basic Script

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -1,2 +1,49 @@
-// app js
 "use strict";
+
+// app js
+var questionButtons = document.querySelectorAll(".question__button");
+window.addEventListener("DOMContentLoaded", function () {
+  questionButtons.forEach(function (button) {
+    button.addEventListener("click", function () {
+      if (button.getAttribute("aria-expanded") == "false") {
+        questionButtons.forEach(function (button) {
+          closeAnswer(button);
+        });
+        openAnswer(button);
+      } else {
+        closeAnswer(button);
+      }
+    });
+  });
+
+  function closeAnswer(button) {
+    var answerContainer = document.getElementById(button.getAttribute("aria-controls"));
+    var answer = answerContainer.querySelector("p");
+    answerContainer.setAttribute("style", "");
+    answerContainer.classList.remove("expanded");
+    answer.classList.remove("visible");
+    button.setAttribute("aria-expanded", "false");
+    button.querySelector(".bx").classList.remove("bx-minus");
+    button.querySelector(".bx").classList.add("bx-plus");
+  }
+
+  function openAnswer(button) {
+    var answerContainer = document.getElementById(button.getAttribute("aria-controls"));
+    var answer = answerContainer.querySelector("p");
+    answerContainer.style.height = "".concat(answer.offsetHeight, "px");
+    answerContainer.classList.add("expanded");
+    answer.classList.add("visible");
+    button.setAttribute("aria-expanded", "true");
+    button.querySelector(".bx").classList.remove("bx-plus");
+    button.querySelector(".bx").classList.add("bx-minus");
+  }
+});
+window.addEventListener("resize", function () {
+  questionButtons.forEach(function (button) {
+    if (button.getAttribute("aria-expanded") == "true") {
+      var answerContainer = document.getElementById(button.getAttribute("aria-controls"));
+      var answer = answerContainer.querySelector("p");
+      answerContainer.style.height = "".concat(answer.offsetHeight, "px");
+    }
+  });
+});
--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,47 @@
 // app js
+const questionButtons = document.querySelectorAll(".question__button");
+
+window.addEventListener("DOMContentLoaded", () => {
+    questionButtons.forEach(button => {
+        button.addEventListener("click", () => {
+            if(button.getAttribute("aria-expanded") == "false") {
+                questionButtons.forEach(button => {
+                    closeAnswer(button);
+                });
+                openAnswer(button);
+            } else {
+                closeAnswer(button);
+            }
+        })
+    })
+    function closeAnswer(button) {
+        let answerContainer = document.getElementById(button.getAttribute("aria-controls"));
+        let answer = answerContainer.querySelector("p");
+        answerContainer.setAttribute("style", "");
+        answerContainer.classList.remove("expanded");
+        answer.classList.remove("visible");
+        button.setAttribute("aria-expanded", "false");
+        button.querySelector(".bx").classList.remove("bx-minus");
+        button.querySelector(".bx").classList.add("bx-plus");
+    }
+    function openAnswer(button) {
+        let answerContainer = document.getElementById(button.getAttribute("aria-controls"));
+        let answer = answerContainer.querySelector("p");
+        answerContainer.style.height = `${answer.offsetHeight}px`;
+        answerContainer.classList.add("expanded");
+        answer.classList.add("visible");
+        button.setAttribute("aria-expanded", "true");
+        button.querySelector(".bx").classList.remove("bx-plus");
+        button.querySelector(".bx").classList.add("bx-minus");
+    }
+})
+
+window.addEventListener("resize", () => {
+    questionButtons.forEach(button => {
+        if(button.getAttribute("aria-expanded") == "true") {
+            let answerContainer = document.getElementById(button.getAttribute("aria-controls"));
+            let answer = answerContainer.querySelector("p");
+            answerContainer.style.height = `${answer.offsetHeight}px`;
+        }
+    })
+})
